5809f8071c70cf41ec1c2ec3ab6f76e2143464e3
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-10-19 13:57:42 +0600

    complete slider

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,37 +1,31 @@
 import React from "react";
-import image5 from '../img/image5.jpg'
+import image2 from '../img/image2.png'
 const Home = () => {
   return (
-    <div className="px-24 pt-20">
-      <section class="text-gray-600 body-font">
-        <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Making the most of the ever-growing 
+    <div className="md:px-36 px-5  h-screen  ">
+      <section class=" body-font md:pt-24">
+        <div class="container mx-auto flex  py-24 md:flex-row flex-col-reverse  items-center">
+          <div class="lg:flex-grow md:w-2/5 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 class="title-font sm:text-4xl text-3xl mb-2  font-bold text-[#1F2E35]">
+              Making the most of the <br /> ever-growing 
             </h1>
-            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-[#EE4248]">
               Information Technology 
             </h1>
-            <p class="mb-8 leading-relaxed">
-              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
-              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
-              park mlkshk tote bag selvage hot chicken authentic tumeric
-              truffaut hexagon try-hard chambray.
+            <p class="mb-8 leading-relaxed text-lg">
+             Managed by a team of professional experts with extensive experience and impressive track records 
             </p>
             <div class="flex justify-center">
-              <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                Button
-              </button>
-              <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                Button
+              <button class="inline-flex text-white bg-[#EE4248] border-0 py-2 px-6 focus:outline-none rounded ">
+                Read More
               </button>
             </div>
           </div>
-          <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <div class="lg:max-w-lg lg:w-full md:w-3/5 mb-5 md:mb-0  w-5/6">
             <img
-              class="object-cover object-center rounded"
+              class="w-[1500px]"
               alt="hero"
-              src={image5}
+              src={image2}
             />
           </div>
         </div>
